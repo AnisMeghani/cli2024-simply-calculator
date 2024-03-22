4a5c154af4915cf044bba3840356a9fafc0b710f
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-23 01:35:13 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter first number",
+    type: "number",
+    name: "firstnumber",
+  },
+  { message: "Enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "Select one of the operators to perform operation!",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", 'Square root', 'Cube root'],
+  },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else if (answer.operator === "Square root") {
+  console.log(answer.firstnumber * answer.firstnumber , answer.secondnumber * answer.secondnumber);
+} else if (answer.operator === "Cube root") {
+    console.log(answer.firstnumber * answer.firstnumber * answer.firstnumber , answer.secondnumber * answer.secondnumber * answer.secondnumber);
+} else {
+  console.log("Please select valid operator");
+}
